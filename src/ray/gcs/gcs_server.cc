f4770e986694e836c1b409dec641110fe0e84b63
Steve Alexander <sca@anyscale.com>
2026-07-20 03:09:40 +0000

    [core] add scaffolding for recovering node resource state
    Signed-off-by: Steve Alexander <sca@anyscale.com>

--- src/ray/gcs/gcs_server.cc
+++ src/ray/gcs/gcs_server.cc
@@ -328,7 +328,7 @@ void GcsServer::GetOrGenerateClusterId(
 }
 
 void GcsServer::DoStart(const GcsInitData &gcs_init_data) {
-  InitClusterResourceScheduler();
+  InitClusterResourceScheduler(gcs_init_data);
   InitGcsNodeManager(gcs_init_data);
   InitClusterLeaseManager();
   InitGcsResourceManager(gcs_init_data);
@@ -555,7 +555,7 @@ void GcsServer::InitGcsResourceLoadPuller() {
       "RayletLoadPulled");
 }
 
-void GcsServer::InitClusterResourceScheduler() {
+void GcsServer::InitClusterResourceScheduler(const GcsInitData &gcs_init_data) {
   cluster_resource_scheduler_ = std::make_shared<ClusterResourceScheduler>(
       // See https://github.com/ray-project/ray/pull/65271 for why the GCS
       // resource view does not need the periodic reset that raylets run.
@@ -567,6 +567,8 @@ void GcsServer::InitClusterResourceScheduler() {
       /*resource_usage_gauge=*/metrics_.resource_usage_gauge,
       /*clock=*/clock_,
       /*is_local_node_with_raylet=*/false);
+
+  cluster_resource_scheduler_->RestoreNodeResources(gcs_init_data.NodeResources());
 }
 
 void GcsServer::InitClusterLeaseManager() {
