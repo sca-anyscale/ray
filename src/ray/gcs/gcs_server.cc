1f799837dabd8354e27a7842ba0d8a377248631c
Steve Alexander <sca@anyscale.com>
2026-09-06 03:09:32 +0000

    [core] flush GCS debug statistics when shutting down
    Signed-off-by: Steve Alexander <sca@anyscale.com>

--- src/ray/gcs/gcs_server.cc
+++ src/ray/gcs/gcs_server.cc
@@ -426,6 +426,9 @@ void GcsServer::Stop() {
           /*service_name=*/"", false);
     }
 
+    // flush state to logs before exiting
+    PrintDebugState();
+
     // Flush any remaining events before stopping.
     if (ray_event_recorder_) {
       ray_event_recorder_->StopExportingEvents();
