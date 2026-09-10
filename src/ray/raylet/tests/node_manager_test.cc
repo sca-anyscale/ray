c6c30cbe69c4028c3ac8c5bb5ab95c7ce8be7510
Steve Alexander <sca@anyscale.com>
2026-09-10 20:23:28 +0000

    [core] move metric recording to a separate Boost context
    Signed-off-by: Steve Alexander <sca@anyscale.com>
    (cherry picked from commit 1486da47feb1e2ba50d10afd2539a76ea312ec6c)

--- src/ray/raylet/tests/node_manager_test.cc
+++ src/ray/raylet/tests/node_manager_test.cc
@@ -471,7 +471,8 @@ class NodeManagerTest : public ::testing::Test {
         boost::asio::basic_stream_socket<local_stream_protocol>(io_service_),
         fake_memory_manager_worker_eviction_total_count_,
         fake_node_manager_unexpected_worker_failure_total_count_,
-        fake_clock_);
+        fake_clock_,
+        metric_context_);
   }
 
   instrumented_io_context io_service_;
@@ -483,6 +484,7 @@ class NodeManagerTest : public ::testing::Test {
   std::unique_ptr<pubsub::FakeSubscriber> core_worker_subscriber_;
   FakeClock fake_clock_;
   ray::Clock clock_;
+  boost::asio::io_context metric_context_;
   std::unique_ptr<RayletClusterResourceStorage> cluster_resource_storage_;
   std::unique_ptr<ClusterResourceScheduler> cluster_resource_scheduler_;
   std::unique_ptr<LocalLeaseManager> local_lease_manager_;
